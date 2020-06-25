23208f82a46b90612021ff04a7f71c9c6998c584
Júnior de Oliveira <ellismarjr@hotmail.com>
2020-06-24 21:41:20 -0300

    feat: user can create appointments

--- src/database/migrations/1592264508575-CreateAppointments.ts
+++ src/database/migrations/1592264508575-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1592264508575
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
