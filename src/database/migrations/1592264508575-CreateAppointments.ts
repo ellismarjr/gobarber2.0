11206c385cb91117a51d4537a1c0bfd62cdc506b
Júnior de Oliveira <ellismarjr@hotmail.com>
2020-06-25 21:39:49 -0300

    refactor: change id type to uuid

--- src/database/migrations/1592264508575-CreateAppointments.ts
+++ src/database/migrations/1592264508575-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1592264508575
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
