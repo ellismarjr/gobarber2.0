e8491549db723de05661d83502fe72b4707d4ff9
Júnior de Oliveira <ellismarjr@hotmail.com>
2020-06-30 22:37:59 -0300

    feat: hash password

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,6 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
 import User from '../models/Users';
 
 interface Request {
@@ -16,10 +18,11 @@ class CreateUserService {
     if (checkUserExists) {
       throw new Error('Email already used!');
     }
+    const hashedPassword = await hash(password, 8);
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await usersRepository.save(user);
